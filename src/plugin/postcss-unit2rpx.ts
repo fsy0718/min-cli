449de9fb17070746da480d41ed970a97248758b6
fsy0718 <fsy0718@gmail.com>
2018-04-17 15:17:12 +0800

    fix(source): 修复px与rpx之间的转换

--- src/plugin/postcss-unit2rpx.ts
+++ src/plugin/postcss-unit2rpx.ts
@@ -6,9 +6,9 @@ export const postcssUnit2rpx = postcss.plugin('postcss-unit2rpx', (options: any)
       root.walkDecls(decl => {
         decl.value = decl.value.replace(/([0-9.]+)(px|rem)/ig, (match, size, unit) => {
           if (unit === 'px') { // 100px => 100rpx
-            return `${size}rpx`
+            return `${size * 2}rpx`
           } else if (unit === 'rem') { // 1rem => 100rpx
-            return `${size * 100}rpx`
+            throw new Error('请不要用rem单位')
           }
           return match
         })
